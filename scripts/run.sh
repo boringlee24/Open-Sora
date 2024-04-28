4cefb01cfe3ba5a812642d33bb97c2c35149d3f1
Baolin Li <baolin.li1994@gmail.com>
2024-04-28 10:12:02 -0400

    Update .gitignore and add new data files

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,12 +6,25 @@ FRAMES=16
 BATCH=1
 STEPS=100
 
+HEIGHT_WIDTH_PAIRS=(
+    "144 256"
+    "360 640"
+    "480 854"
+    "720 1280"
+    "1080 1920"
+)
+
 set -x
 
-python inference_singleGPU.py "../configs/opensora-v1-1/inference/sample.py" \
-    --ckpt-path OpenSora-STDiT-v2-stage3/model.safetensors \
-    --prompt-path ../assets/texts/char.txt \
-    --image-size $HEIGHT $WIDTH \
-    --num-frames $FRAMES \
-    --batch-size $BATCH \
-    --num-sampling-steps $STEPS 
+for pair in "${HEIGHT_WIDTH_PAIRS[@]}"
+do
+    HEIGHT=$(echo "$pair" | cut -d ' ' -f1)
+    WIDTH=$(echo "$pair" | cut -d ' ' -f2)
+    python inference_singleGPU.py "../configs/opensora-v1-1/inference/sample.py" \
+        --ckpt-path OpenSora-STDiT-v2-stage3/model.safetensors \
+        --prompt-path ../assets/texts/char.txt \
+        --image-size $HEIGHT $WIDTH \
+        --num-frames $FRAMES \
+        --batch-size $BATCH \
+        --num-sampling-steps $STEPS
+done
